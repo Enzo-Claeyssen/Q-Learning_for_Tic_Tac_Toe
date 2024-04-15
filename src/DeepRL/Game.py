aaa98ace802a1eea9fbc672d55a2ace473eb1cdb
Enzo Claeyssen <enzo.claeyssen.etu@univ-lille.fr>
2024-04-15 20:58:14 +0200

    Done everything but play

--- src/DeepRL/Game.py
+++ src/DeepRL/Game.py
@@ -4,27 +4,49 @@ from .opponent.Opponent import Opponent
 class Game() :
     """ This class represents Game instances """
     
-    def __init__(board, opp1, opp2) :
+    def __init__(self, board, opp1, opp2) :
         """
         Creates a new game
         :param: board The board where the game will take place
         :param: opp1 The first opponent
         :param: opp2 The second opponent
         """
-        pass
+        self.__BOARD = board
+        self.__OPPONENT1 = opp1
+        self.__OPPONENT2 = opp2
     
     
-    def isFinished() :
+    def isFinished(self) :
         """
         Verifies if the game is finished
         :returns: True if the game is finished, False otherwise
         """
-        pass
+        return not self.getWinner() is None
     
     
-    def getWinner() :
+    def getWinner(self) :
         """
         Retrieves the winner of the game
         :returns: The opponent object representing the winner
         """
-        pass
+        owner_grid = [[self.__BOARD.getCell(x, y).getOwner() for x in range(3)] for y in range(3)]
+        
+        equality_verification = [[(0, 0), (0, 1), (0, 2)], # This contains a set of coordinates corresponding to a line
+                                 [(1, 0), (1, 1), (1, 2)],
+                                 [(2, 0), (2, 1), (2, 2)],
+                                 [(0, 0), (1, 0), (2, 0)],
+                                 [(0, 1), (1, 1), (2, 1)],
+                                 [(0, 2), (1, 2), (2, 2)],
+                                 [(0, 0), (1, 1), (2, 2)],
+                                 [(2, 0), (1, 1), (0, 2)]]
+        
+        for case in equality_verification :
+            aligned = True
+            x, y = case[0]
+            opp = self.__BOARD.getCell(x, y).getOwner()
+            for (x, y) in case[1:] :
+                if opp != self.__BOARD.getCell(x, y).getOwner() :
+                    aligned = False
+            if aligned :
+                return opp
+        return None
