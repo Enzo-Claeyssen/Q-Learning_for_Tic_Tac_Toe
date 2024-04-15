eaed1493ddca57de70ca7b7461dec3b11e9e5fb1
Enzo Claeyssen <enzo.claeyssen.etu@univ-lille.fr>
2024-04-15 20:56:21 +0200

    Fixed tests

--- src/DeepRL/test_Game.py
+++ src/DeepRL/test_Game.py
@@ -12,53 +12,53 @@ class testGame(unittest.TestCase) :
         self.game = Game(self.board, self.opp1, self.opp2);
     
     def test_isFinishedHonrizontal(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(0, 1, opp1)
-        self.board.capture(0, 2, opp1)
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(0, 1, self.opp1)
+        self.board.capture(0, 2, self.opp1)
         self.assertTrue(self.game.isFinished())
     
     def test_isFinishedVertical(self) :
-        self.board.capture(0, 0, opp2)
-        self.board.capture(1, 0, opp2)
-        self.board.capture(2, 0, opp2)
+        self.board.capture(0, 0, self.opp2)
+        self.board.capture(1, 0, self.opp2)
+        self.board.capture(2, 0, self.opp2)
         self.assertTrue(self.game.isFinished())
     
     def test_isFinishedDiagonal(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(1, 1, opp1)
-        self.board.capture(2, 2, opp1)
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(1, 1, self.opp1)
+        self.board.capture(2, 2, self.opp1)
         self.assertTrue(self.game.isFinished())
     
     def test_isFinishedReturnsFalse_RandomPlay(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(0, 1, opp2)
-        self.board.capture(2, 2, opp1)
-        self.board.capture(1, 0, opp2)
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(0, 1, self.opp2)
+        self.board.capture(2, 2, self.opp1)
+        self.board.capture(1, 0, self.opp2)
         self.assertFalse(self.game.isFinished())
     
     def test_isFinishedReturnsFalse_LineFilled(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(1, 1, opp1)
-        self.board.capture(2, 2, opp2) # Opp2 blocks the line
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(1, 1, self.opp1)
+        self.board.capture(2, 2, self.opp2) # Opp2 blocks the line
         self.assertFalse(self.game.isFinished())
     
     
     def test_getWinner_opp1WinsHorizontal(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(0, 1, opp1)
-        self.board.capture(0, 2, opp1)
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(0, 1, self.opp1)
+        self.board.capture(0, 2, self.opp1)
         self.assertEqual(self.opp1, self.game.getWinner())
     
     def test_getWinner_opp2WinsVertical(self) :
-        self.board.capture(0, 0, opp2)
-        self.board.capture(1, 0, opp2)
-        self.board.capture(2, 0, opp2)
+        self.board.capture(0, 0, self.opp2)
+        self.board.capture(1, 0, self.opp2)
+        self.board.capture(2, 0, self.opp2)
         self.assertEqual(self.opp2, self.game.getWinner())
     
     def test_getWinner_returnsNullNoWinner(self) :
-        self.board.capture(0, 0, opp1)
-        self.board.capture(1, 1, opp1)
-        self.board.capture(2, 2, opp2) # Opp2 blocks the line
+        self.board.capture(0, 0, self.opp1)
+        self.board.capture(1, 1, self.opp1)
+        self.board.capture(2, 2, self.opp2) # Opp2 blocks the line
         self.assertIsNone(self.game.getWinner())
 
 
